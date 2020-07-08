55d4cb143c96d176aa6ffdd1c119181ecbb16dad
gunayanil <gunayanilemre@gmail.com>
2020-07-08 00:05:35 -0400

    basic router structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/signin'>
+          <p>Sign in</p>
+        </Route>
+        <Route path='/signup'>
+          <p>Sign up</p>
+        </Route>
+        <Route path='/browse'>
+          <p>Browse</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
