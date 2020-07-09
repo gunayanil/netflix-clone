d349c287423f9c86d911f0f696a19c9ef17f39e2
gunayanil <gunayanilemre@gmail.com>
2020-07-09 16:50:46 -0400

    added footer

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form } from '../components';
 import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
 
 export const Signin = () => {
   const [error, setError] = useState('');
@@ -15,33 +16,36 @@ export const Signin = () => {
   };
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign In</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
-        <Form.Base onSubmit={handleSignin} method='POST'>
-          <Form.Input
-            placeholder='Email address'
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-          />
-          <Form.Input
-            type='password'
-            value={password}
-            placeholder='Password'
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <Form.Submit disabled={isInvalid} type='submit'>
-            Sign In
-          </Form.Submit>
-          <Form.Text>
-            New to Netflix? <Form.Link to='/signup'>Sign up now</Form.Link>
-          </Form.Text>
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA.
-          </Form.TextSmall>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method='POST'>
+            <Form.Input
+              placeholder='Email address'
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              type='password'
+              value={password}
+              placeholder='Password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type='submit'>
+              Sign In
+            </Form.Submit>
+            <Form.Text>
+              New to Netflix? <Form.Link to='/signup'>Sign up now</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 };
