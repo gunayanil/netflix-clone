66c757e8e0fb2dd01d8d947a59c009fe7d99c552
gunayanil <gunayanilemre@gmail.com>
2020-07-09 13:15:59 -0400

    added routes as constants

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 import { Home } from './pages/home';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/' exact>
+        <Route path={ROUTES.HOME} exact>
           <Home />
         </Route>
-        <Route path='/signin'>
+        <Route path={ROUTES.SIGN_IN}>
           <p>Sign in</p>
         </Route>
-        <Route path='/signup'>
+        <Route path={ROUTES.SIGN_UP}>
           <p>Sign up</p>
         </Route>
-        <Route path='/browse'>
+        <Route path={ROUTES.BROWSE}>
           <p>Browse</p>
         </Route>
       </Switch>
