97439f4672d3b9e3f0e8bd40b166d81209bd89ed
gunayanil <gunayanilemre@gmail.com>
2020-07-11 14:36:13 -0400

    header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,8 +5,13 @@ import {
   ButtonLink,
   Container,
   Frame,
+  Group,
   Link,
   Logo,
+  Text,
+  Feature,
+  FeatureCallOut,
+  PlayButton,
 } from './styles/header';
 
 export const Header = ({ bg = true, children, ...props }) => {
@@ -21,10 +26,34 @@ Header.Frame = function HeaderFrame({ children, ...props }) {
   return <Container {...props}>{children}</Container>;
 };
 
+Header.Group = function HeaderGroup({ children, ...props }) {
+  return <Group {...props}>{children}</Group>;
+};
+
 Header.Logo = function HeaderLogo({ to, ...props }) {
   return (
     <ReactRouterLink to={to}>
       <Logo {...props} />
     </ReactRouterLink>
   );
+};
+
+Header.Link = function HeaderLink({ children, ...props }) {
+  return <Link {...props}>{children}</Link>;
+};
+
+Header.Text = function HeaderText({ children, ...props }) {
+  return <Text {...props}>{children}</Text>;
+};
+
+Header.Feature = function HeaderFeautre({ children, ...props }) {
+  return <Feature {...props}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeautreCallOut({ children, ...props }) {
+  return <FeatureCallOut {...props}>{children}</FeatureCallOut>;
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...props }) {
+  return <PlayButton {...props}>{children}</PlayButton>;
 };
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
 import { FirebaseContext } from '../context/firebase';
@@ -7,6 +7,10 @@ import { FooterContainer } from './footer';
 
 export const BrowseContainer = () => {
   const [profile, setProfile] = useState({});
+  const [category, setCategory] = useState('series');
+  const [loadng, setLoading] = useState(true);
+
+  const { firebase } = useContext(FirebaseContext);
 
   const user = {
     displayName: 'Anil',
@@ -15,7 +19,40 @@ export const BrowseContainer = () => {
 
   return profile.displayName ? (
     <>
-      <p>Browse Container</p>
+      <Header src='joker1' dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo
+              to={ROUTES.HOME}
+              src='/images/misc/logo.svg'
+              alt='Netflix'
+            />
+            <Header.Link
+              active={category === 'series' ? 'true' : 'false'}
+              onClick={() => setCategory('series')}
+            >
+              Series
+            </Header.Link>
+            <Header.Link
+              active={category === 'movies' ? 'true' : 'false'}
+              onClick={() => setCategory('movies')}
+            >
+              Movies
+            </Header.Link>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallout>Watch Joker Now</Header.FeatureCallout>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
       <FooterContainer />
     </>
   ) : (
