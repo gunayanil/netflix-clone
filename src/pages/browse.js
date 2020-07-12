e44982925cb4a92831ea9bfa0cd15d3e20f19268
gunayanil <gunayanilemre@gmail.com>
2020-07-11 20:24:16 -0400

    imported useContent hook for future

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
 
 export const Browse = () => {
   return <BrowseContainer />;
